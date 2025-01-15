4dce7abd3073ce0bc0d17c2bc943fa5da1f16f8b
Khairun-Nahar-Munne <khairunnaharmunne81@gmail.com>
2025-01-15 08:57:28 +0600

    ?

--- controllers/property_controller.go
+++ controllers/property_controller.go
@@ -5,7 +5,6 @@ import (
     "rentApi/models"
     "rentApi/services"
     "rentApi/utils"
-    "sync"
 
     beego "github.com/beego/beego/v2/server/web"
 )
@@ -17,6 +16,8 @@ type PropertyController struct {
 // /v1/property/list
 func (c *PropertyController) Get() {
     propertyService := services.PropertyService{}
+    
+    // Get locations
     locations, err := propertyService.GetLocations()
     if err != nil {
         c.Data["json"] = map[string]interface{}{"error": err.Error()}
@@ -26,80 +27,63 @@ func (c *PropertyController) Get() {
 
     var response models.PropertyListResponse
     response.Success = true
+    response.Locations = make([]models.LocationWithProperties, 0)
 
-    var wg sync.WaitGroup
-    locationChan := make(chan models.LocationWithProperties, len(locations))
-
+    // Process each location serially
     for _, location := range locations {
-        wg.Add(1)
-        go func(location models.Location) {
-            defer wg.Done()
-            rentalProperties, err := propertyService.GetRentalProperties(int(location.Id))
+        // Get rental properties for current location
+        rentalProperties, err := propertyService.GetRentalProperties(int(location.Id))
+        if err != nil {
+            continue
+        }
+
+        properties := make([]models.PropertyResponse, 0)
+
+        // Process each rental property serially
+        for _, rentalProperty := range rentalProperties {
+            // Get property details
+            propertyDetails, err := propertyService.GetPropertyDetails(rentalProperty.Id)
             if err != nil {
-                return
+                continue
             }
 
-            var properties []models.PropertyResponse
-            var propertyWg sync.WaitGroup
-            propertyChan := make(chan models.PropertyResponse, len(rentalProperties))
+            // Prepare property data
+            images := utils.PrepareImages(propertyDetails)
+            breadcrumbs := utils.PrepareBreadcrumbs(rentalProperty)
+            displayLocation := utils.SplitDisplayLocation(rentalProperty.DisplayLocation)
+            amenities := utils.SplitAmenities(rentalProperty.Amenities)
 
-            for _, rentalProperty := range rentalProperties {
-                propertyWg.Add(1)
-                go func(rentalProperty models.RentalProperty) {
-                    defer propertyWg.Done()
-                    propertyDetails, err := propertyService.GetPropertyDetails(rentalProperty.Id)
-                    if err != nil {
-                        return
-                    }
-
-                    images := utils.PrepareImages(propertyDetails)
-                    breadcrumbs := utils.PrepareBreadcrumbs(rentalProperty)
-                    displayLocation := utils.SplitDisplayLocation(rentalProperty.DisplayLocation)
-                    amenities := utils.SplitAmenities(rentalProperty.Amenities)
-
-                    property := models.PropertyResponse{
-                        Id:              rentalProperty.Id,
-                        PropertyId:      rentalProperty.PropertyId,
-                        PropertySlugId:  rentalProperty.PropertySlugId,
-                        HotelName:       rentalProperty.HotelName,
-                        Bedrooms:        rentalProperty.Bedrooms,
-                        Bathrooms:       rentalProperty.Bathrooms,
-                        GuestCount:      rentalProperty.GuestCount,
-                        Rating:          rentalProperty.Rating,
-                        ReviewCount:     rentalProperty.ReviewCount,
-                        Price:           rentalProperty.Price,
-                        Breadcrumbs:     breadcrumbs,
-                        DisplayLocation: displayLocation,
-                        Amenities:       amenities,
-                        Type:            rentalProperty.Type,
-                        Images:          images,
-                    }
-                    propertyChan <- property
-                }(rentalProperty)
+            // Create property response
+            property := models.PropertyResponse{
+                Id:              rentalProperty.Id,
+                PropertyId:      rentalProperty.PropertyId,
+                PropertySlugId:  rentalProperty.PropertySlugId,
+                HotelName:       rentalProperty.HotelName,
+                Bedrooms:        rentalProperty.Bedrooms,
+                Bathrooms:       rentalProperty.Bathrooms,
+                GuestCount:      rentalProperty.GuestCount,
+                Rating:          rentalProperty.Rating,
+                ReviewCount:     rentalProperty.ReviewCount,
+                Price:           rentalProperty.Price,
+                Breadcrumbs:     breadcrumbs,
+                DisplayLocation: displayLocation,
+                Amenities:       amenities,
+                Type:           rentalProperty.Type,
+                Images:         images,
             }
 
-            propertyWg.Wait()
-            close(propertyChan)
+            properties = append(properties, property)
+        }
 
-            for property := range propertyChan {
-                properties = append(properties, property)
-            }
+        // Create location with properties
+        locationWithProperties := models.LocationWithProperties{
+            Id:         location.Id,
+            DestId:     location.DestId,
+            DestType:   location.DestType,
+            Value:      location.Value,
+            Properties: properties,
+        }
 
-            locationWithProperties := models.LocationWithProperties{
-                Id:         location.Id,
-                DestId:     location.DestId,
-                DestType:   location.DestType,
-                Value:      location.Value,
-                Properties: properties,
-            }
-            locationChan <- locationWithProperties
-        }(location)
-    }
-
-    wg.Wait()
-    close(locationChan)
-
-    for locationWithProperties := range locationChan {
         response.Locations = append(response.Locations, locationWithProperties)
     }
 
